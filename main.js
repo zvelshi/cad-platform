d98216b5e1cb5921ad558d9013f8bea7439bd930
Zac Velshi <zvelshi@gmail.com>
2023-06-19 14:28:59 -0400

    wipe for alpha-2

--- main.js
+++ main.js
@@ -9,8 +9,8 @@ let folderPath = '';
 
 const createWindow = () => {
   mainWindow = new BrowserWindow({
-    width: 1050,
-    height: 1050,
+    width: 1280,
+    height: 720,
     webPreferences: {
       nodeIntegration: true,
       contextIsolation: false
@@ -18,56 +18,6 @@ const createWindow = () => {
   })
 
   mainWindow.loadFile('index.html')
-
-  ipcMain.handle('open-folder-dialog', async (event) => {
-    const result = await dialog.showOpenDialog(mainWindow, {
-      properties: ['openDirectory']
-    });
-  
-    if (!result.canceled) {
-      const selectedFolderPath = result.filePaths[0];
-  
-      // Make sure to set the folderPath to selectedFolderPath
-      folderPath = selectedFolderPath;
-
-      if (watcher) {
-        watcher.close();
-      }
-
-      watcher = chokidar.watch(folderPath, {
-        ignoreInitial: true,
-        events: ['add', 'change', 'unlink'] // Include 'add' event
-      });
-
-      watcher.on('all', (event, filePath) => {
-        mainWindow.webContents.send('file-changed', filePath, event);
-      });
-
-        const hierarchy = await getFolderHierarchy(folderPath);
-      return hierarchy;
-    } else {
-      folderPath = '';
-      if (watcher) {
-        watcher.close();
-        watcher = null;
-      }
-    }
-
-    return [];
-  });
-
-  ipcMain.handle('get-folder-hierarchy', async () => {
-    if (folderPath) {
-      try {
-        const hierarchy = await getFolderHierarchy(folderPath);
-        return hierarchy;
-      } catch (error) {
-        console.error('Error reading folder hierarchy:', error);
-      }
-    }
-
-    return [];
-  });
 }
 
 app.whenReady().then(() => {
@@ -82,22 +32,3 @@ app.whenReady().then(() => {
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') app.quit()
 })
-
-async function getFolderHierarchy(folderPath) {
-  const stats = await fs.lstat(folderPath);
-  const hierarchy = {
-    name: path.basename(folderPath),
-    path: folderPath,
-    type: stats.isDirectory() ? 'folder' : 'file',
-    isModified: false,
-  };
-
-  if (stats.isDirectory()) {
-    const children = await fs.readdir(folderPath);
-    hierarchy.children = await Promise.all(children.map(async (child) => {
-      const childPath = path.join(folderPath, child);
-      return getFolderHierarchy(childPath);
-    }));
-  }
-  return hierarchy;
-}
