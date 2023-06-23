3ef46ad2e43bd5c20ed54eb238f05c7eab2b5e7f
Zac Velshi <zvelshi@gmail.com>
2023-06-22 23:16:09 -0400

    pull
    added pull and clone cloud repo to local dir

--- main.js
+++ main.js
@@ -3,10 +3,11 @@ const { s3 } = require('./s3_client.js');
 const { dbdoc } = require('./db_client.js');
 const Store = require('electron-store');
 const { v4: uuidv4 } = require('uuid');
+const path = require('path');
 const fs = require('fs');
 
-const { CreateBucketCommand } = require('@aws-sdk/client-s3');
-const { PutCommand, GetCommand } = require('@aws-sdk/lib-dynamodb');
+const { CreateBucketCommand, GetObjectCommand, ListObjectsV2Command } = require('@aws-sdk/client-s3');
+const { PutCommand, GetCommand, ScanCommand } = require('@aws-sdk/lib-dynamodb');
 
 require('electron-reload')(__dirname, {
   electron: require(`${__dirname}/node_modules/electron`)
@@ -84,18 +85,7 @@ const createWindow = () => {
   });
 
   ipcMain.handle('get-active-repo', async (event) => {
-    const activeRepo = store.get('activeRepo');
-    if (activeRepo) {
-      const res = await dbdoc.send(new GetCommand({
-        TableName: 'repositories',
-        Key: {
-          id: activeRepo,
-        }
-      }));
-      return res.Item;
-    } else {
-      return null;
-    }
+    return await getActiveRepo();
   });
 
   ipcMain.handle('set-active-repo', async (event, uniqueName) => {
@@ -103,25 +93,64 @@ const createWindow = () => {
   });
 
   ipcMain.handle('get-local-repos', async (event) => {
-    const localRepos = [];
-    const config = store.store;
-    for (const key in config) {
-      let obj = { uniqueName: '', friendlyName: '', organization: '', folderPath: '' };
-      if (key !== 'activeRepo') {
-        obj.uniqueName = key;
-        obj.friendlyName = config[key].friendlyName;
-        obj.organization = config[key].organization;
-        obj.folderPath = config[key].folderPath;
-        localRepos.push(obj);        
-      }
-    }
-    return localRepos;
+    return await getLocalRepos();
   });
 
-  //for debug purposes only
+  ipcMain.handle('get-cloud-repos', async (event) => {
+    const res = await dbdoc.send(new ScanCommand({
+      TableName: 'repositories',
+    }));
+    return res.Items;
+  });
+
+  ipcMain.handle('clone-repo', async (event, repoUniqueName, folderPathWithoutFName) => {
+    //get data from dynamodb
+    const res = await dbdoc.send(new GetCommand({
+      TableName: 'repositories',
+      Key: {
+        id: repoUniqueName,
+      }
+    }));
+
+    const repoData = {
+      uniqueName: repoUniqueName,
+      friendlyName: res.Item.friendlyName,
+      organization: res.Item.organization,
+      folderPath: folderPathWithoutFName
+    }
+
+    //create local json entry 
+    store.set(repoUniqueName, {
+      friendlyName: repoData.friendlyName,
+      organization: repoData.organization,
+      folderPath: repoData.folderPath
+    });
+    
+    //clone s3 bucket
+    const hierarchy = await getBucketHierarchyClone(repoData.uniqueName);
+    cloneCloudRepo(hierarchy, repoData.folderPath, repoData.uniqueName);
+
+    //set active repo
+    store.set('activeRepo', repoUniqueName);
+  });
+
+  ipcMain.handle('pull-repo', async (event) => {
+    const activeRepo = store.get('activeRepo');
+    const localRepos = await getLocalRepos();
+
+    for (let i = 0; i < localRepos.length; i++) {
+      if(localRepos[i].uniqueName === activeRepo) {
+        const hierarchy = await getBucketHierarchyClone(localRepos[i].uniqueName);
+        cloneCloudRepo(hierarchy, localRepos[i].folderPath, localRepos[i].uniqueName);
+      }
+    }
+  });
+
+  //start of debug section
   ipcMain.on('clear-json', (event) => {
     store.clear();
   });
+  //end of debug section
 };
 
 app.whenReady().then(() => {
@@ -134,3 +163,131 @@ app.whenReady().then(() => {
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') app.quit();
 });
+
+async function cloneCloudRepo(hierarchy, folderPath, bucketName) {
+  const destinationPath = path.join(folderPath, hierarchy.name);
+
+  if (hierarchy.type === 'file') {
+    // Check if the file already exists at the destination
+    if (!fs.existsSync(destinationPath)) {
+      const localFilePath = await getFileContentFromS3(hierarchy.path, destinationPath, bucketName);
+
+      // Create parent directories recursively
+      const parentDir = path.dirname(localFilePath);
+      await fs.promises.mkdir(parentDir, { recursive: true });
+
+    }
+  } else if (hierarchy.type === 'folder') {
+
+    if (!fs.existsSync(destinationPath)) {
+      await fs.promises.mkdir(destinationPath, { recursive: true });
+    }
+    for (const child of hierarchy.children) {
+      await cloneCloudRepo(child, destinationPath, bucketName);
+    }
+  }
+}
+
+async function getFileContentFromS3(filePath, localFilePath, bucketName) {
+  const params = {
+    Bucket: bucketName,
+    Key: filePath,
+  };
+  
+  try {
+    const data = await s3.send(new GetObjectCommand(params));
+    const stream = data.Body;
+
+    await new Promise((resolve, reject) => {
+      const fileStream = fs.createWriteStream(localFilePath);
+      stream.pipe(fileStream);
+
+      stream.on('end', resolve);
+      stream.on('error', reject);
+    });
+
+    return localFilePath;
+  } catch (err) {
+    console.error('Error getting file content from S3:', err);
+    return null;
+  }  
+}
+
+async function getBucketHierarchyClone(bucketName) {
+  const res = await s3.send(new ListObjectsV2Command ({ 
+    Bucket: bucketName 
+  }));
+  const contents = res.Contents;
+
+  const hierarchy = {
+    name: bucketName,
+    path: '',
+    type: 'folder',
+    isModified: false,
+    children: [],
+  };
+
+  for (const content of contents) {
+    const key = content.Key;
+    const parts = key.split('/');
+
+    let currentFolder = hierarchy;
+    for (let i = 0; i < parts.length - 1; i++) {
+      const folderName = parts[i];
+      let childFolder = currentFolder.children.find((child) => child.name === folderName);
+      if (!childFolder) {
+        childFolder = {
+          name: folderName,
+          path: parts.slice(0, i + 1).join('/'),
+          type: 'folder',
+          isModified: false,
+          children: [],
+        };
+        currentFolder.children.push(childFolder);
+      }
+      currentFolder = childFolder;
+    }
+
+    const fileName = parts[parts.length - 1];
+    const file = {
+      name: fileName,
+      path: key,
+      type: 'file',
+      isModified: false,
+    };
+    currentFolder.children.push(file);
+  }
+
+  return hierarchy;
+}
+
+async function getActiveRepo(){
+  const activeRepo = store.get('activeRepo');
+  if (activeRepo) {
+    const res = await dbdoc.send(new GetCommand({
+      TableName: 'repositories',
+      Key: {
+        id: activeRepo,
+      }
+    }));
+    return res.Item;
+  } else {
+    return null;
+  }
+}
+
+async function getLocalRepos(){
+  const localRepos = [];
+  const config = store.store;
+  for (const key in config) {
+    let obj = { uniqueName: '', friendlyName: '', organization: '', folderPath: '' };
+    if (key !== 'activeRepo') {
+      obj.uniqueName = key;
+      obj.friendlyName = config[key].friendlyName;
+      obj.organization = config[key].organization;
+      obj.folderPath = config[key].folderPath;
+      localRepos.push(obj);        
+    }
+  }
+  return localRepos;
+}
--- renderer.js
+++ renderer.js
@@ -1,6 +1,10 @@
-const { ipcRenderer } = require('electron');
+const { ipcRenderer, ipcMain } = require('electron');
 
 document.addEventListener('DOMContentLoaded', () => {
+  const org = document.getElementById('organization');
+
+  const selectRepoBtn = document.getElementById('repo-select-btn');
+
   const selectDirBtn = document.getElementById('select-dir-btn');
   const selectDirBtn2 = document.getElementById('select-dir-btn2');
   const selectDirBtn3 = document.getElementById('select-dir-btn3');
@@ -10,6 +14,12 @@ document.addEventListener('DOMContentLoaded', () => {
 
   const changeRepoSelect = document.getElementById('change-repo-select');
   const changeRepoSelectBtn = document.getElementById('change-repo-select-btn');
+
+  const cloneRepoCloudSelect = document.getElementById('clone-repo-cloud-select');
+  const cloneRepoCloudBtn = document.getElementById('clone-repo-cloud-btn');
+
+  const pushBtn = document.getElementById('push-btn');
+  const pullBtn = document.getElementById('pull-btn');
 
   ipcRenderer.invoke('get-active-repo').then((activeRepo) => {
     if (activeRepo){
@@ -45,7 +55,8 @@ document.addEventListener('DOMContentLoaded', () => {
   selectDirBtn2.addEventListener('click', () => {
     ipcRenderer.invoke('open-folder-dialog').then((folderPath) => {
       if (folderPath) {
-        //do something with this folder
+        document.getElementById('clone-repo-cloud-path').textContent = folderPath;
+        document.getElementById('clone-repo-cloud-btn').removeAttribute('disabled', 'disabled');
       }
     });
   });
@@ -58,10 +69,16 @@ document.addEventListener('DOMContentLoaded', () => {
     });
   });
 
+  pullBtn.addEventListener('click', () => {
+    ipcRenderer.invoke('pull-repo').then(() => {
+      alert('pull complete');
+    });
+  });
+
   createRepoCreateBtn.addEventListener('click', () => { 
     const friendlyName = document.getElementById('create-repo-name').value;
     const folderPath = createRepoPathLabel.innerText + '\\' + friendlyName;
-    const organization = document.getElementById('organization').value.toLowerCase();
+    const organization = document.getElementById('organization').value;
 
     const repoData = {
       folderPath,
@@ -71,10 +88,10 @@ document.addEventListener('DOMContentLoaded', () => {
 
     ipcRenderer.send('create-repo', repoData);
     document.getElementById('active-repo').innerText = friendlyName;
+    pullBtn.removeAttribute('disabled', 'disabled');
+    pushBtn.removeAttribute('disabled', 'disabled');
   });
 
-  //for debug purposes only
-  const selectRepoBtn = document.getElementById('repo-select-btn');
   selectRepoBtn.addEventListener('click', () => {
     const val = document.getElementById('repo-select').value;
     
@@ -90,6 +107,20 @@ document.addEventListener('DOMContentLoaded', () => {
       createDiv.setAttribute("hidden", "hidden");
       cloneDiv.removeAttribute("hidden", "hidden");
       pushDiv.setAttribute("hidden", "hidden");
+
+      ipcRenderer.invoke('get-cloud-repos').then((repos) => {
+        const orgRepos = repos.filter((repo) => repo.organization === org.value);
+        cloneRepoCloudSelect.innerHTML = '';
+        
+        orgRepos.forEach((repo) => {
+          console.log(repo);
+          const option = document.createElement('option');
+          option.value = repo.id;
+          option.innerText = repo.friendlyName + ' - ' + repo.organization;
+          cloneRepoCloudSelect.appendChild(option);
+        });
+      });
+
     } else if (val == 3) {
       createDiv.setAttribute("hidden", "hidden");
       cloneDiv.setAttribute("hidden", "hidden");
@@ -97,9 +128,18 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   });
 
+  cloneRepoCloudBtn.addEventListener('click', () => {
+    const repoUniqueName = cloneRepoCloudSelect.options[cloneRepoCloudSelect.selectedIndex].value;
+    const folderPathWithoutFName = document.getElementById('clone-repo-cloud-path').textContent + '\\';
+    ipcRenderer.invoke('clone-repo', repoUniqueName, folderPathWithoutFName);
+    pullBtn.removeAttribute('disabled', 'disabled');
+    pushBtn.removeAttribute('disabled', 'disabled');
+  });
+
+
+  //for debug purposes only
   const clearJSONBtn = document.getElementById('clear-json');
   clearJSONBtn.addEventListener('click', () => {
     ipcRenderer.send('clear-json');
   });
-
 });
